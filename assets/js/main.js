f655cf8df353f07f5ffe99c08b73d30839dbcf03
kevinsupreme900 <kevin.wang@hotmail.it>
2020-10-25 21:11:38 +0100

    final change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
-/*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+// set bg audio
+var vid = document.getElementById("audio");
+vid.volume = 0.1;
 
 (function($) {
 
